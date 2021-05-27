8dde3a01925aff3f407bf2d5534ac6d6909d0044
CosmicAsad <asy_7@hotmail.co.uk>
2021-05-27 18:50:03 +0000

    Added webhook event handlers

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,6 +7,19 @@ class StripeWH_Handler:
         self.request = request
 
     def handle_event(self, event):
+        """Handles a generic/unexpected/unknown webhook event"""
+        return HttpResponse(
+            content=f'Unhandled Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """Handles Stripe payment intent succeeded webhook event"""
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """Handles Stripe payment intent failure webhook event"""
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
--- soundwaves/settings.py
+++ soundwaves/settings.py
@@ -173,6 +173,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', 'pk_test_51IvQ0SDyyvgINJ1Sf73rZogZKD6tYbD8878arLlD8Xn0qBZDYQ0PhQmAxcaXhvXcVMQ11puu2TlismP5XDP2UPLp00S4BnxTBV')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
