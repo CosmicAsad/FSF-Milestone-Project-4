fe85d5b53b138d0e644b4e71f9c2e79af93f2d91
CosmicAsad <asy_7@hotmail.co.uk>
2021-05-29 16:01:31 +0000

    Deployment to Heroku

--- soundwaves/settings.py
+++ soundwaves/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&zq(y@5rr@^o@!(lsxuw1ki34q9cu80%v6q+833emz9f!x-t&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['soundwaves-ms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'soundwaves.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
