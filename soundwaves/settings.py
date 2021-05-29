5710401d73788daa0844b657ce0eec77085b5ee7
CosmicAsad <asy_7@hotmail.co.uk>
2021-05-29 20:18:49 +0000

    Fixed error with default file storage

--- soundwaves/settings.py
+++ soundwaves/settings.py
@@ -192,7 +192,7 @@ if 'USE_AWS' in os.environ:
     # Static and media files for live site
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom.storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
