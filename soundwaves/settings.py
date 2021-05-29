54236dea38d0cb4b76403ee0893b8a5aa17d5fc6
CosmicAsad <asy_7@hotmail.co.uk>
2021-05-29 16:17:45 +0000

    Removed secret key and set debug

--- soundwaves/settings.py
+++ soundwaves/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&zq(y@5rr@^o@!(lsxuw1ki34q9cu80%v6q+833emz9f!x-t&%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['soundwaves-ms4.herokuapp.com', 'localhost']
 
